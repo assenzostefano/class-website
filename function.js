142e26d8ede10579db3c848e5343dc6baf480f2e
stef-devs <stef58a@gmail.com>
2022-04-12 17:35:40 +0200

    Technically I fixed the dark mode

--- function.js
+++ function.js
@@ -16,3 +16,20 @@ var checkbox = document.querySelector('input[name=mode]');
                 document.documentElement.classList.remove('transition');
             }, 1000)
         }
+
+        const btn = document.querySelector(".container_toggle");
+
+        const currentTheme = localStorage.getItem("theme");
+        if (currentTheme == "dark") {
+          document.body.classList.add("dark-theme");
+        }
+        
+        btn.addEventListener("click", function () {
+          document.body.classList.toggle("dark-theme");
+        
+          let theme = "light";
+          if (document.body.classList.contains("dark-theme")) {
+            theme = "dark";
+          }
+          localStorage.setItem("theme", theme);
+        });
