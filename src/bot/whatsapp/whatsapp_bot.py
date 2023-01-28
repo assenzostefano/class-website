3f75138d1651eefa222dd97cc8af2e5cc0c19b8b
Stefano Assenzo <stef58a@gmail.com>
2023-01-28 17:41:45 +0000

    Reorder libraries on app.py

--- src/bot/whatsapp/whatsapp_bot.py
+++ src/bot/whatsapp/whatsapp_bot.py
@@ -8,6 +8,6 @@ PHONE_NUMBER_PERSONAL = os.getenv('PHONE_NUMBER_PERSONAL')
 
 client = Client(SID, AUTH_TOKEN)
 
-message=client.messages.create(to=PHONE_NUMBER_PERSONAL, 
+message = client.messages.create(to=PHONE_NUMBER_PERSONAL, 
                                 from_=PHONE_NUMBER_BOT, 
                                 body="Hello from Python!")
